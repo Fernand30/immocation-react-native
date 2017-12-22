d49546d3eda5bc17b4b98499a37cd54ed5e30613
daniel benz <seegalbird@mail.com>
2017-12-22 00:19:27 -0600

    remove  unnecessary files

--- src/Firebase/FirebaseConstants.js
+++
@@ -1,5 +0,0 @@
-const FirebaseConstants = {
-  KEY: "AIzaSyADczerGhCl7l934vjJQDPH-3hTSw0RnWE"
-}
-
-export default FirebaseConstants;
--- src/screens/FirstScreen/index.js
+++ src/screens/FirstScreen/index.js
@@ -11,9 +11,6 @@ import {
   Platform
 } from 'react-native';
 import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from 'react-native-fcm';
-
-import {registerKilledListener, registerAppListener} from "../../Firebase/Listeners";
-import firebaseClient from  "../../Firebase/FirebaseClient";
 import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
 import { Actions } from 'react-native-router-flux';
 import {Colors, Fonts, Images, Metrics, Constants } from '../../Themes';
@@ -22,39 +19,8 @@ import Header from "../IOS/Header";
 import GraphBox from "../IOS/GraphBox";
 import BookBox from "../IOS/BookBox";
 
-// this shall be called regardless of app state: running, background or not running. Won't be called when app is killed by user in iOS
-FCM.on(FCMEvent.Notification, async (notif) => {
-    // there are two parts of notif. notif.notification contains the notification payload, notif.data contains data payload
-    if(notif.local_notification){
-      //this is a local notification
-    }
-    if(notif.opened_from_tray){
-      //iOS: app is open/resumed because user clicked banner
-      //Android: app is open/resumed because user clicked banner or tapped app icon
-    }
-    // await someAsyncCall();
-
-    if(Platform.OS ==='ios'){
-      //optional
-      //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see https://developer.apple.com/documentation/uikit/uiapplicationdelegate/1623013-application.
-      //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
-      //notif._notificationType is available for iOS platfrom
-      switch(notif._notificationType){
-        case NotificationType.Remote:
-          notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
-          break;
-        case NotificationType.NotificationResponse:
-          notif.finish();
-          break;
-        case NotificationType.WillPresent:
-          notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
-          break;
-      }
-    }
-});
 FCM.on(FCMEvent.RefreshToken, (token) => {
     alert(token)
-    // fcm token may not be available on first load, catch it here
 });
 
 export default class FirstScreen extends Component {
@@ -68,17 +34,11 @@ export default class FirstScreen extends Component {
   }
 
     componentDidMount() {
-        // iOS: show permission prompt for the first call. later just check permission in user settings
-        // Android: check permission in user settings
-        //FCM.requestPermissions().then(()=>console.log('granted')).catch(()=>alert('notification permission rejected'));
-        
         FCM.getFCMToken().then(token => {
             alert(token)
-            // store fcm token in your server
         });
         
     }
-
 
   goRechner(){
     Actions.login();
